a3f277bd3034e8e08e951d662c8af104acca8603
Urvisha17 <113713486+Urvisha17@users.noreply.github.com>
2022-10-17 17:38:46 +0530

    Add files via upload

---
+++ node index.js
@@ -0,0 +1,8 @@
+var http = require("http");
+http.createServer(function(req, res){
+    res.writeHead("200",{"Content-Type":"text/html"});
+    res.write("Welcome to NodeJs");
+    res.end();
+}).listen(9000)
+
+//console.log("Hello Node.js!");
